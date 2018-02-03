1d9d7ba5a48f961698fb769c0ef6bca0aef3d103
Ivan Lim <aimprove@gmail.com>
2018-02-03 11:30:17 -0500

    Replace defaults with app specific info

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <span>{{ appName }}</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -12,7 +11,12 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      appName: 'notes2go'
+    }
+  }
 }
 </script>
 
