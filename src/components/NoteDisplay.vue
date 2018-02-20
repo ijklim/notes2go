153adbeba6b4b1e8b5cc3a60f4b743c08d1e39e0
Ivan Lim <aimprove@gmail.com>
2018-02-20 10:17:43 -0500

    Add progress bar to view page waiting for notes to load

--- src/components/NoteDisplay.vue
+++ src/components/NoteDisplay.vue
@@ -1,8 +1,10 @@
 <template>
   <v-flex xs12 sm10 mt-2>
     <v-card>
-      <v-card-text v-html="formattedNotes">
-      </v-card-text>
+      <v-card-text v-html="formattedNotes" v-if="notes" />
+      <v-card-text v-else class="text-xs-center py-5">
+        <v-progress-circular indeterminate :size="50" color="primary" />
+      </v-card-text >
     </v-card>
   </v-flex>
 </template>
