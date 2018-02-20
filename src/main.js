d134e16b24b8e2c2b46bf40c23d8793c791b7609
Ivan Lim <aimprove@gmail.com>
2018-02-20 10:27:15 -0500

    Add Firebase anonymous authentication

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 
 import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/database'
 
 import Vuetify from 'vuetify'
@@ -26,8 +27,33 @@ const snackbar = makeSnackbar(Vue)
 const store = makeStore(Vue, alert, firebase, router, snackbar)
 
 // console.log('[Dev Only]', 'Vue Version: ', Vue.version)
-Vue.config.productionTip = true
+Vue.config.productionTip = false
 // Vue.config.devtools = false
+
+/**
+ * Initialize Firebase
+ */
+const _initDB = () => {
+  firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  })
+}
+
+/**
+ * Sign in user anonymously
+ */
+const _initUser = () => {
+  if (firebase.auth().currentUser) return
+
+  firebase.auth().signInAnonymously()
+    .catch(error => {
+      console.error('Firebase anonymously sign in error, please check Firebase rules', error)
+    })
+}
 
 /* eslint-disable no-new */
 new Vue({
@@ -39,12 +65,7 @@ new Vue({
   template: '<App/>',
   components: { App },
   created () {
-    firebase.initializeApp({
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-    })
+    _initDB()
+    _initUser()
   }
 })
